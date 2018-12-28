97d5a0d2491a9477856d419748fae63d533e9f63
Robert W Ferney <robert.w.ferney@uscis.dhs.gov>
2018-12-28 19:05:44 +0000

    adding in basic commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,7 @@
-#
 # Cookbook:: node_n
 # Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'npm'
+user_execute "npm install -g n"
+user_execute "n 8.14.0"
+
---
+++ resources/user_execute.rb
@@ -0,0 +1,33 @@
+# To learn more about Custom Resources, see https://docs.chef.io/custom_resources.html
+resource_name :user_execute
+
+property :command, String, name_property: true
+property :user, String, default: lazy{ |r| r.name || ::Etc.getpwuid(1000).name }
+property :usr, Etc::Passwd, default: lazy{ |r| ::Etc.getpwnam(r.user) }
+property :grp, Etc::Group, default: lazy{ |r| ::Etc.getgrgid(r.usr.gid) }
+property :group, String, default: lazy {|r| r.grp.name }
+property :home, String, default: lazy {|r| r.usr.dir }
+property :cwd, String, default: lazy {|r| r.home }
+property :environment, Hash, default: lazy { |r|
+      { 
+        'HOME': r.home, 
+        'USER': r.user, 
+        'USERNAME': r.user, 
+        'LOGNAME': r.user,
+        'PATH': "/usr/local/bin:/usr/bin:/bin"
+      }
+    }
+
+action :execute do
+  execute 'install rvm' do
+    cwd new_resource.cwd
+    user new_resource.user
+    group new_resource.group
+    environment new_resource.environment
+    command new_resource.command
+    creates new_resource.installed_file
+    live_stream true
+  end
+end
+
+
